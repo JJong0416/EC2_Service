50d27ca00532e172be0a572d5b7e62b0a2a5986b
jjong0416-tech <whdcks5681@naver.com>
2021-07-11 00:58:53 +0900

    Paging & Keyword Searching

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -16,43 +16,28 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    /* 게시글 목록 출력 */
+    @GetMapping("/")
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "1") Integer pageNum) {
+        List<BoardDto> boardList = boardService.getBoardlist(pageNum);
+        Integer[] pageList = boardService.getPageList(pageNum);
+
+        model.addAttribute("boardList",boardList);
+        model.addAttribute("pageList",pageList);
+
+        return "board/list.html";
+    }
+    
+    /* 게시글 상세*/
     @GetMapping("/post/{no}")
-    public String detail(@PathVariable("no") Long no, Model model){
+    public String detail(@PathVariable Long no, Model model){
         BoardDto boardDTO = boardService.getPost(no);
 
         model.addAttribute("boardDto",boardDTO);
         return "board/detail.html";
     }
 
-    @GetMapping("/post/edit/{no}")
-    public String edit(@PathVariable("no") Long no, Model model){
-        BoardDto boardDTO = boardService.getPost(no);
-        model.addAttribute("boardDto",boardDTO);
-        return "board/update.html";
-    }
-
-    @PutMapping("/post/edit/{no}")
-    public String update(BoardDto boardDto){
-        boardService.savePost(boardDto);
-        return "redirect:/";
-    }
-
-    @DeleteMapping("/post/{no}")
-    public String delete(@PathVariable("no") Long no){
-        boardService.deletePost(no);
-        return "redirect:/";
-    }
-
-
-    /* 게시글 목록 출력 */
-    @GetMapping("/")
-    public String list(Model model) {
-        List<BoardDto> boardList = boardService.getBoardlist();
-
-        model.addAttribute("boardList",boardList);
-        return "board/list.html";
-    }
-
+    /* 게시글 쓰기 */
     @GetMapping("/post")
     public String write() {
         return "board/write.html";
@@ -62,5 +47,36 @@ public class BoardController {
     public String write(BoardDto boardDto) {
         boardService.savePost(boardDto);
         return "redirect:/";
+    }
+
+    /* 게시글 수정 */
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable Long no, Model model) {
+        BoardDto boardDTO = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDTO);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDTO) {
+        boardService.savePost(boardDTO);
+
+        return "redirect:/";
+    }
+
+    /* 게시글 삭제 */
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable Long no){
+        boardService.deletePost(no);
+        return "redirect:/";
+    }
+
+    /* 게시글 검색 */
+    @GetMapping("/board/search")
+    public String search(@RequestParam(value = "keyword") String keyword, Model model){
+        List<BoardDto> boardDtoList = boardService.searchPosts(keyword);
+        model.addAttribute("boardList",boardDtoList);
+        return "board/list.html";
     }
 }
--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -5,8 +5,10 @@ import com.example.demo.domain.entity.BoardEntity;
 import com.example.demo.dto.BoardDto;
 import com.example.demo.domain.repository.BoardRepository;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,25 +19,54 @@ import java.util.Optional;
 @AllArgsConstructor
 @Service
 public class BoardService   {
+
+    private static final int BLOCK_PAGE_NUM_COUNT = 5; // 블럭에 존재하는 페이지 번호 수
+    private static final int PAGE_POST_COUNT =4; // 한 페이지에 존재하는 게시글 수
+
     private final BoardRepository boardRepository;
 
     @Transactional
-    public List<BoardDto> getBoardlist(){
-        List<BoardEntity> boardEntities = boardRepository.findAll();
+    public List<BoardDto> getBoardlist(Integer pageNum){
+        Page<BoardEntity> page = boardRepository.findAll(PageRequest.of(pageNum-1,PAGE_POST_COUNT
+                , Sort.by(Sort.Direction.ASC,"createdDate")));
+
+        List<BoardEntity> boardEntites = page.getContent();
         List<BoardDto> boardDtoList = new ArrayList<>();
 
-        for (BoardEntity boardEntity : boardEntities){
-            BoardDto boardDto = BoardDto.builder()
-                    .id(boardEntity.getId())
-                    .title(boardEntity.getTitle())
-                    .content(boardEntity.getContent())
-                    .writer(boardEntity.getWriter())
-                    .createdDate(boardEntity.getCreatedDate())
-                    .build();
-
-            boardDtoList.add(boardDto);
+        for (BoardEntity boardEntity : boardEntites){
+            boardDtoList.add(this.convertEntityToDto(boardEntity));
         }
         return boardDtoList;
+    }
+
+    @Transactional
+    public Long getBoardCount(){
+        return boardRepository.count();
+    }
+
+    @Transactional
+    public Integer[] getPageList(Integer curPageNum){
+        Integer[] pageList = new Integer[BLOCK_PAGE_NUM_COUNT];
+
+        // 총 게시글 갯수
+        Double postsTotalCount = Double.valueOf(this.getBoardCount());
+
+        // 총 게시물 기준으로 계산한 마지막 페이지 번호 계산 (올림으로 계산)
+        Integer totalLastPageNum = (int)(Math.ceil(postsTotalCount/PAGE_POST_COUNT));
+
+        // 현재 페이지를 기준으로 블럭의 마지막 페이지 번호 계산
+        Integer blockLastPageNum = (totalLastPageNum > curPageNum + BLOCK_PAGE_NUM_COUNT)
+                ? curPageNum + BLOCK_PAGE_NUM_COUNT
+                : totalLastPageNum;
+
+        // 페이지 시작 번호 조정
+        curPageNum = (curPageNum<=3)? 1 : curPageNum - 2;
+
+        // 페이지 번호 할당
+        for(int val = curPageNum, idx = 0; val <= blockLastPageNum; val++, idx++){
+            pageList[idx] = val;
+        }
+        return pageList;
     }
 
     @Transactional
@@ -57,6 +88,31 @@ public class BoardService   {
                 .build();
 
         return boardDTO;
+    }
+
+    @Transactional
+    public List<BoardDto> searchPosts(String keyword){
+        List<BoardEntity> boardEntities = boardRepository.findByTitleContaining(keyword);
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        if (boardEntities.isEmpty())
+            return boardDtoList;
+
+        for (BoardEntity boardEntity : boardEntities) {
+            boardDtoList.add(this.convertEntityToDto(boardEntity));
+        }
+
+        return boardDtoList;
+    }
+
+    private BoardDto convertEntityToDto(BoardEntity boardEntity){
+        return BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getTitle())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
     }
 
     @Transactional
